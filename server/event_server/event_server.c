ac3b27b552c4c8a197826ffe26e330de7977f6dc
Dev <devendra.aaru@outlook.com>
2023-03-31 19:31:34 +0530

    changes in firewall
    - temporary settings for event testing
    
    Signed-off-by: Dev <devendra.aaru@outlook.com>

--- server/event_server/event_server.c
+++ server/event_server/event_server.c
@@ -1,7 +1,46 @@
 #include <event_server.h>
 
+int fw_event_server_init(const char *ipaddr, int port)
+{
+    struct sockaddr_in serv;
+    int ret;
+    int fd;
+
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd < 0) {
+        return -1;
+    }
+
+    serv.sin_addr.s_addr = inet_addr(ipaddr);
+    serv.sin_port = htons(port);
+    serv.sin_family = AF_INET;
+
+    ret = bind(fd, (struct sockaddr *)&serv, sizeof(serv));
+    if (ret < 0) {
+        goto bind_err;
+    }
+
+    while (1) {
+        char pkt[4096];
+
+        ret = recvfrom(fd, pkt, sizeof(pkt), 0, NULL, NULL);
+        if (ret < 0) {
+            break;
+        }
+        printf("received %d bytes\n", ret);
+    }
+
+bind_err:
+    if (fd > 0) {
+        close(fd);
+    }
+
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
+    fw_event_server_init("127.0.0.1", 2022);
     return 0;
 }
 
---
+++ server/event_server/event_server.h
@@ -0,0 +1,11 @@
+#ifndef __FW_EVENT_SERVER_H__
+#define __FW_EVENT_SERVER_H__
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#endif
+
