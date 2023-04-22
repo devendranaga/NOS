b5f35e17413fd6496b1c890e2d853aabfef6a650
Dev <devendra.aaru@outlook.com>
2023-04-22 06:05:10 -0700

    Fix more dhcp option parsing
    Signed-off-by: Dev <devendra.aaru@outlook.com>

---
+++ src/lib/protocols/fw_ports.h
@@ -0,0 +1,13 @@
+/**
+ * @brief - Defines ports.
+ *
+ * @author - Devendra Naga (devendra.aaru@outlook.com).
+ * @copyright - 2023-present All rights reserved.
+ */
+#ifndef __FW_PORTS_H__
+#define __FW_PORTS_H__
+
+#define FW_PORT_DHCP_SRC_PORT 67
+#define FW_PORT_DHCP_DST_PORT 68
+
+#endif
