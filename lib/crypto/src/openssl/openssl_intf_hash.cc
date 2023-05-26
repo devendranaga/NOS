c01d84a62dd6f90e5764a91b8ac878118ec34a08
Dev <devendra.aaru@outlook.com>
2023-05-26 10:45:44 -0700

    support openssh sha2 and sha3 interfaces for hashing a buffer
    - add test code to test the sha256
    
    Signed-off-by: Dev <devendra.aaru@outlook.com>

--- lib/crypto/src/openssl/openssl_intf_hash.cc
+++ lib/crypto/src/openssl/openssl_intf_hash.cc
@@ -4,10 +4,48 @@
 
 namespace nos::crypto {
 
+static crypto_error hash_msg(hash_input_buf &in,
+                             hash_output &out,
+                             const EVP_MD *md)
+{
+    crypto_error err = crypto_error::SHA2_256_FAILURE;
+    EVP_MD_CTX *md_ctx;
+    int ret;
+
+    md_ctx = EVP_MD_CTX_new();
+    if (!md_ctx) {
+        return err;
+    }
+
+    ret = EVP_DigestInit_ex(md_ctx, md, NULL);
+    if (ret != 1) {
+        goto free_md_ctx;
+    }
+
+    ret = EVP_DigestUpdate(md_ctx, in.buf, in.buf_size);
+    if (ret != 1) {
+        goto free_md_ctx;
+    }
+
+    ret = EVP_DigestFinal(md_ctx, out.hash, &out.hash_size);
+    if (ret != 1) {
+        goto free_md_ctx;
+    }
+
+    err = crypto_error::NO_ERROR;
+
+free_md_ctx:
+    if (md_ctx != NULL) {
+        EVP_MD_CTX_free(md_ctx);
+    }
+
+    return err;
+}
+
 crypto_error openssl_hash::sha2_256(hash_input_buf &in,
                                     hash_output &out)
 {
-    return crypto_error::NO_ERROR;
+    return hash_msg(in, out, EVP_sha256());
 }
 
 crypto_error openssl_hash::sha2_256(const std::string &in_file,
@@ -19,7 +57,7 @@ crypto_error openssl_hash::sha2_256(const std::string &in_file,
 crypto_error openssl_hash::sha2_384(hash_input_buf &in,
                                     hash_output &out)
 {
-    return crypto_error::NO_ERROR;
+    return hash_msg(in, out, EVP_sha384());
 }
 
 crypto_error openssl_hash::sha2_384(const std::string &in_file,
@@ -31,7 +69,7 @@ crypto_error openssl_hash::sha2_384(const std::string &in_file,
 crypto_error openssl_hash::sha2_512(hash_input_buf &in,
                                     hash_output &out)
 {
-    return crypto_error::NO_ERROR;
+    return hash_msg(in, out, EVP_sha512());
 }
 
 crypto_error openssl_hash::sha2_512(const std::string &in_file,
@@ -43,7 +81,7 @@ crypto_error openssl_hash::sha2_512(const std::string &in_file,
 crypto_error openssl_hash::sha3_256(hash_input_buf &in,
                                     hash_output &out)
 {
-    return crypto_error::NO_ERROR;
+    return hash_msg(in, out, EVP_sha3_256());
 }
 
 crypto_error openssl_hash::sha3_256(const std::string &in_file,
@@ -55,7 +93,7 @@ crypto_error openssl_hash::sha3_256(const std::string &in_file,
 crypto_error openssl_hash::sha3_384(hash_input_buf &in,
                                     hash_output &out)
 {
-    return crypto_error::NO_ERROR;
+    return hash_msg(in, out, EVP_sha3_384());
 }
 
 crypto_error openssl_hash::sha3_384(const std::string &in_file,
@@ -67,7 +105,7 @@ crypto_error openssl_hash::sha3_384(const std::string &in_file,
 crypto_error openssl_hash::sha3_512(hash_input_buf &in,
                                     hash_output &out)
 {
-    return crypto_error::NO_ERROR;
+    return hash_msg(in, out, EVP_sha3_512());
 }
 
 crypto_error openssl_hash::sha3_512(const std::string &in_file,
@@ -79,7 +117,7 @@ crypto_error openssl_hash::sha3_512(const std::string &in_file,
 crypto_error openssl_hash::ripemd160(hash_input_buf &in,
                                      hash_output &out)
 {
-    return crypto_error::NO_ERROR;
+    return hash_msg(in, out, EVP_ripemd160());
 }
 
 crypto_error openssl_hash::ripemd160(const std::string &in_file,
