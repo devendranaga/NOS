8decaa7b6822aeefe576e8949dbe85e91b5b33db
Dev <devendra.aaru@outlook.com>
2023-07-01 10:16:23 -0700

    implement #25 uds parsing
    Signed-off-by: Dev <devendra.aaru@outlook.com>

--- firewall/include/packet-doip.h
+++ firewall/include/packet-doip.h
@@ -16,7 +16,8 @@
 
 #define PACKET_DOIP_PORT 13400
 
-#define DOIP_TYPE_VEH_ANNOUNCEMENT 0x0004
+#define DOIP_TYPE_VEH_ANNOUNCEMENT  0x0004
+#define DOIP_TYPE_DIAGNOSTICS_MSG   0x8001
 
 namespace nos::firewall
 {
@@ -29,12 +30,28 @@ struct doip_veh_announcement_msg {
     uint8_t     further_action_required;
 };
 
+#define DOIP_DIAG_SESSION_CTRL 0x10
+
+struct uds_diagnostics_session_control {
+    uint8_t type;
+};
+
+struct doip_diagnostics_msg {
+    uint16_t    source_addr;
+    uint16_t    target_addr;
+    uint8_t     service_id;
+    bool        reply_flag;
+
+    uds_diagnostics_session_control sess_ctrl;
+};
+
 struct doip_header {
     uint8_t     version;
     uint8_t     inv_version;
     uint16_t    type;
     uint32_t    len;
     doip_veh_announcement_msg announcement;
+    doip_diagnostics_msg diag;
 
     event_type deserialize(packet_buf &buf,
                            const std::shared_ptr<nos::core::logging> &log);
--- firewall/src/packet-doip.cc
+++ firewall/src/packet-doip.cc
@@ -17,12 +17,26 @@ event_type doip_header::deserialize(packet_buf &buf,
     buf.deserialize_2_bytes(&type);
     buf.deserialize_4_bytes(&len);
 
-    if (type == DOIP_TYPE_VEH_ANNOUNCEMENT) {
-        buf.deserialize_bytes(announcement.vin, sizeof(announcement.vin));
-        buf.deserialize_2_bytes(&announcement.logical_addr);
-        buf.deserialize_bytes(announcement.eid, sizeof(announcement.eid));
-        buf.deserialize_bytes(announcement.gid, sizeof(announcement.gid));
-        buf.deserialize_byte(&announcement.further_action_required);
+    switch (type) {
+        case DOIP_TYPE_VEH_ANNOUNCEMENT: {
+            buf.deserialize_bytes(announcement.vin, sizeof(announcement.vin));
+            buf.deserialize_2_bytes(&announcement.logical_addr);
+            buf.deserialize_bytes(announcement.eid, sizeof(announcement.eid));
+            buf.deserialize_bytes(announcement.gid, sizeof(announcement.gid));
+            buf.deserialize_byte(&announcement.further_action_required);
+        } break;
+        case DOIP_TYPE_DIAGNOSTICS_MSG: {
+            buf.deserialize_2_bytes(&diag.source_addr);
+            buf.deserialize_2_bytes(&diag.target_addr);
+            buf.deserialize_byte(&diag.service_id);
+            diag.reply_flag = !!(buf.data[buf.off - 1] & 0x40);
+
+            switch (diag.service_id) {
+                case DOIP_DIAG_SESSION_CTRL: {
+                    buf.deserialize_byte(&diag.sess_ctrl.type);
+                } break;
+            }
+        }
     }
 
     return event_type::NO_ERROR;
