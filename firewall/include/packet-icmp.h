5765ef0e4dc653ae9f30d124313a9b9ec909767d
Dev <devendra.aaru@outlook.com>
2023-07-08 23:43:31 -0700

    support for icmp redirect
    Signed-off-by: Dev <devendra.aaru@outlook.com>

--- firewall/include/packet-icmp.h
+++ firewall/include/packet-icmp.h
@@ -4,12 +4,49 @@
 #include <stdint.h>
 #include <event_types.h>
 #include <packet-buf.h>
+#include <packet-ipv4.h>
+#include <packet-udp.h>
 
 namespace nos::firewall
 {
 
-#define PING_REQ 0x08
-#define PING_REPLY 0x00
+/*
+ * ICMP Types.
+ */
+#define ICMP_REPLY                          0
+#define ICMP_DEST_UNREACHABLE               3
+#define ICMP_REDIRECT                       5
+#define ICMP_REQ                            8
+#define ICMP_ROUTER_ADV                     9
+#define ICMP_ROUTER_SOL                     10
+#define ICMP_TIME_EXCEED                    11
+#define ICMP_PARAMETER_PROB                 12
+#define ICMP_TIMESTAMP                      13
+#define ICMP_TIMESTAMP_REPLY                14
+
+/*
+ * ICMP Codes.
+ */
+#define ICMP_REPLY_CODE                     0
+
+#define ICMP_DEST_NW_UNREACHABLE            0
+#define ICMP_DEST_HOST_UNREACHABLE          1
+#define ICMP_DEST_PROTO_UNREACHABLE         2
+#define ICMP_DEST_PORT_UNREACHABLE          3
+#define ICMP_DEST_FRAG_NEEDED               4
+#define ICMP_SOURCE_ROUTE_FAILED            5
+
+#define ICMP_REDIR_FOR_NW                   0
+#define ICMP_REDIR_DATAGRAM_FOR_HOST        1
+#define ICMP_REDIR_DATAGRAM_FOR_TOS_NW      2
+#define ICMP_REDIR_DATAGRAM_FOR_SVC_HOST    3
+
+#define ICMP_TTL_EXCEEDED_IN_TRANSIT        0
+#define ICMP_FRAG_REASSMEBLY_TIME_EXCEEDED  1
+
+#define ICMP_PTR_IND_ERR                    0
+#define ICMP_MISSING_REQ_OPT                1
+#define ICMP_BAD_LEN                        2
 
 struct icmp_ping_request {
     uint16_t id;
@@ -21,13 +58,23 @@ struct icmp_ping_reply {
     uint16_t seq_no;
 };
 
+struct icmp_redirect {
+    uint32_t ipaddr;
+    ipv4_header ipv4_hdr;
+    uint8_t datagram_data[8];
+
+    event_type deserialize(packet_buf &buf);
+    void print();
+};
+
 struct icmp_header {
     uint8_t type;
     uint8_t code;
     uint16_t checksum;
 
-    icmp_ping_request ping_req;
-    icmp_ping_reply ping_reply;
+    icmp_ping_request   ping_req;
+    icmp_ping_reply     ping_reply;
+    icmp_redirect       redir;
 
     event_type deserialize(packet_buf &buf);
     void print();
--- firewall/src/packet-icmp.cc
+++ firewall/src/packet-icmp.cc
@@ -3,18 +3,40 @@
 namespace nos::firewall
 {
 
+event_type icmp_redirect::deserialize(packet_buf &buf)
+{
+    event_type type;
+
+    buf.deserialize_4_bytes(&ipaddr);
+    type = ipv4_hdr.deserialize(buf);
+    if (type != event_type::NO_ERROR) {
+        return type;
+    }
+
+    buf.deserialize_bytes(datagram_data, sizeof(datagram_data));
+
+    return event_type::NO_ERROR;
+}
+
 event_type icmp_header::deserialize(packet_buf &buf)
 {
+    event_type evt_type;
+
     buf.deserialize_byte(&type);
     buf.deserialize_byte(&code);
     buf.deserialize_2_bytes(&checksum);
 
-    if (type == PING_REQ) {
+    if (type == ICMP_REQ) {
         buf.deserialize_2_bytes(&ping_req.id);
         buf.deserialize_2_bytes(&ping_req.seq_no);
-    } else if (type == PING_REPLY) {
+    } else if (type == ICMP_REPLY) {
         buf.deserialize_2_bytes(&ping_reply.id);
         buf.deserialize_2_bytes(&ping_reply.seq_no);
+    } else {
+        evt_type = redir.deserialize(buf);
+        if (evt_type != event_type::NO_ERROR) {
+            return evt_type;
+        }
     }
 
     return event_type::NO_ERROR;
