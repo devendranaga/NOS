04a1f935cc6e6ae74ea96c0d396ffb831cc37c5d
Dev <devendra.aaru@outlook.com>
2023-07-10 02:33:24 -0700

    support mqtt message types
    Signed-off-by: Dev <devendra.aaru@outlook.com>

--- firewall/include/packet-mqtt.h
+++ firewall/include/packet-mqtt.h
@@ -9,7 +9,14 @@
 namespace nos::firewall
 {
 
-#define MQTT_MSG_TYPE_CONNECT 1
+#define MQTT_MSG_TYPE_CONNECT       1
+#define MQTT_MSG_TYPE_CONNECT_ACK   2
+#define MQTT_MSG_TYPE_PUBLISH       3
+#define MQTT_MSG_TYPE_SUB_REQ       8
+#define MQTT_MSG_TYPE_SUB_ACK       9
+#define MQTT_MSG_TYPE_PING_REQ      12
+#define MQTT_MSG_TYPE_PING_RESP     13
+#define MQTT_MSG_TYPE_DISCONN_REQ   14
 
 struct mqtt_connect_flags {
     bool username_flag;
@@ -22,6 +29,9 @@ struct mqtt_connect_flags {
 };
 
 struct mqtt_connect_header {
+    uint16_t protocol_name_len;
+    uint8_t protocol_name[16];
+    uint8_t version;
     mqtt_connect_flags flags;
     uint16_t keep_alive;
     uint16_t client_id_len;
@@ -32,14 +42,82 @@ struct mqtt_connect_header {
     void free_hdr() { }
 };
 
+struct mqtt_connect_ack_header {
+    uint8_t reserved;
+    uint8_t return_code;
+
+    event_type deserialize(packet_buf &buf,
+                           const std::shared_ptr<nos::core::logging> &log);
+    void free_hdr() { }
+};
+
+struct mqtt_sub_req_header {
+    uint16_t msg_id;
+    uint16_t topic_len;
+    uint8_t topic[32];
+    uint8_t req_qos;
+
+    event_type deserialize(packet_buf &buf,
+                           const std::shared_ptr<nos::core::logging> &log);
+    void free_hdr() { }
+};
+
+struct mqtt_sub_ack_header {
+    uint16_t msg_id;
+    uint8_t granted_qos;
+
+    event_type deserialize(packet_buf &buf,
+                           const std::shared_ptr<nos::core::logging> &log);
+    void free_hdr() { }
+};
+
+struct mqtt_publish_msg_header {
+    uint16_t topic_len;
+    uint8_t topic[32];
+    uint8_t msg[1024];
+
+    event_type deserialize(packet_buf &buf,
+                           const std::shared_ptr<nos::core::logging> &log);
+    void free_hdr() { }
+};
+
+struct mqtt_ping_req_header {
+    uint8_t msg_len;
+
+    event_type deserialize(packet_buf &buf,
+                           const std::shared_ptr<nos::core::logging> &log);
+    void free_hdr() { }
+};
+
+struct mqtt_ping_resp_header {
+    uint8_t msg_len;
+
+    event_type deserialize(packet_buf &buf,
+                           const std::shared_ptr<nos::core::logging> &log);
+    void free_hdr() { }
+};
+
+struct mqtt_disconn_req_header {
+    uint8_t msg_len;
+
+    event_type deserialize(packet_buf &buf,
+                           const std::shared_ptr<nos::core::logging> &log);
+    void free_hdr() { }
+};
+
 struct mqtt_header {
     uint8_t msg_type;
     uint8_t reserved;
     uint32_t msg_len;
-    uint16_t protocol_name_len;
-    uint8_t protocol_name[16];
-    uint8_t version;
     mqtt_connect_header connect_hdr;
+    mqtt_connect_ack_header connect_ack_hdr;
+    mqtt_sub_req_header sub_req_hdr;
+    mqtt_sub_ack_header sub_ack_hdr;
+    mqtt_publish_msg_header pub_msg;
+    mqtt_ping_req_header ping_req;
+    mqtt_ping_resp_header ping_resp;
+    mqtt_disconn_req_header disconn_req;
+
     event_type deserialize(packet_buf &buf,
                            const std::shared_ptr<nos::core::logging> &log);
     void free_hdr() { }
--- firewall/src/packet-mqtt.cc
+++ firewall/src/packet-mqtt.cc
@@ -6,6 +6,10 @@ namespace nos::firewall
 event_type mqtt_connect_header::deserialize(packet_buf &buf,
                                             const std::shared_ptr<nos::core::logging> &log)
 {
+    buf.deserialize_2_bytes(&protocol_name_len);
+    buf.deserialize_bytes(protocol_name, protocol_name_len);
+    buf.deserialize_byte(&version);
+
     flags.username_flag = !!(buf.data[buf.off] & 0x80);
     flags.password_flag = !!(buf.data[buf.off] & 0x40);
     flags.will_retain = !!(buf.data[buf.off] & 0x20);
@@ -23,6 +27,77 @@ event_type mqtt_connect_header::deserialize(packet_buf &buf,
     return event_type::NO_ERROR;
 }
 
+event_type mqtt_connect_ack_header::deserialize(packet_buf &buf,
+                                                const std::shared_ptr<nos::core::logging> &log)
+{
+    buf.deserialize_byte(&reserved);
+    buf.deserialize_byte(&return_code);
+
+    return event_type::NO_ERROR;
+}
+
+event_type mqtt_sub_req_header::deserialize(packet_buf &buf,
+                                            const std::shared_ptr<nos::core::logging> &log)
+{
+    buf.deserialize_2_bytes(&msg_id);
+    buf.deserialize_2_bytes(&topic_len);
+    if (topic_len > 0) {
+        buf.deserialize_bytes(topic, topic_len);
+    }
+    buf.deserialize_byte(&req_qos);
+
+    return event_type::NO_ERROR;
+}
+
+event_type mqtt_sub_ack_header::deserialize(packet_buf &buf,
+                                            const std::shared_ptr<nos::core::logging> &log)
+{
+    buf.deserialize_2_bytes(&msg_id);
+    buf.deserialize_byte(&granted_qos);
+
+    return event_type::NO_ERROR;
+}
+
+event_type mqtt_publish_msg_header::deserialize(packet_buf &buf,
+                                                const std::shared_ptr<nos::core::logging> &log)
+{
+    uint16_t remaining_bytes;
+
+    buf.deserialize_2_bytes(&topic_len);
+    if (topic_len > 0) {
+        buf.deserialize_bytes(topic, topic_len);
+    }
+
+    remaining_bytes = buf.remaining_bytes();
+    buf.deserialize_bytes(msg, remaining_bytes);
+
+    return event_type::NO_ERROR;
+}
+
+event_type mqtt_ping_req_header::deserialize(packet_buf &buf,
+                                             const std::shared_ptr<nos::core::logging> &log)
+{
+    buf.deserialize_byte((uint8_t *)&msg_len);
+
+    return event_type::NO_ERROR;
+}
+
+event_type mqtt_ping_resp_header::deserialize(packet_buf &buf,
+                                              const std::shared_ptr<nos::core::logging> &log)
+{
+    buf.deserialize_byte((uint8_t *)&msg_len);
+
+    return event_type::NO_ERROR;
+}
+
+event_type mqtt_disconn_req_header::deserialize(packet_buf &buf,
+                                                const std::shared_ptr<nos::core::logging> &log)
+{
+    buf.deserialize_byte((uint8_t *)&msg_len);
+
+    return event_type::NO_ERROR;
+}
+
 event_type mqtt_header::deserialize(packet_buf &buf,
                                     const std::shared_ptr<nos::core::logging> &log)
 {
@@ -32,13 +107,31 @@ event_type mqtt_header::deserialize(packet_buf &buf,
     reserved = buf.data[buf.off] & 0x0F;
     /* TODO: Fix message length decode. */
     buf.deserialize_byte((uint8_t *)&msg_len);
-    buf.deserialize_2_bytes(&protocol_name_len);
-    buf.deserialize_bytes(protocol_name, protocol_name_len);
-    buf.deserialize_byte(&version);
 
     switch (msg_type) {
         case MQTT_MSG_TYPE_CONNECT:
             type = connect_hdr.deserialize(buf, log);
+        break;
+        case MQTT_MSG_TYPE_CONNECT_ACK:
+            type = connect_ack_hdr.deserialize(buf, log);
+        break;
+        case MQTT_MSG_TYPE_SUB_REQ:
+            type = sub_req_hdr.deserialize(buf, log);
+        break;
+        case MQTT_MSG_TYPE_SUB_ACK:
+            type = sub_ack_hdr.deserialize(buf, log);
+        break;
+        case MQTT_MSG_TYPE_PUBLISH:
+            type = pub_msg.deserialize(buf, log);
+        break;
+        case MQTT_MSG_TYPE_PING_REQ:
+            type = ping_req.deserialize(buf, log);
+        break;
+        case MQTT_MSG_TYPE_PING_RESP:
+            type = ping_resp.deserialize(buf, log);
+        break;
+        case MQTT_MSG_TYPE_DISCONN_REQ:
+            type = disconn_req.deserialize(buf, log);
         break;
         default:
             return event_type::MQTT_UKNOWN_MSG_TYPE;
